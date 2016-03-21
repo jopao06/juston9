886fc1e9783c236b91d4d7308d61778b15a67003
Juan Paulo Bañares <jopao06@gmail.com>
2016-03-21 19:37:23 +0800

    Updated modal for product and added login page

---
+++ js/index.js
@@ -0,0 +1,105 @@
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('.dropdown-toggle').dropdown()
+
+  // Duplicate Image Panel Template 
+  var displayNum = 16;
+  var thumbnail;
+  for (var i = 0; i < displayNum; i++) {
+    thumbnail = $('#thumbnail').clone();
+    thumbnail
+      .attr("id","thumbnail"+i) // Assign unique id
+      .find("img")
+      .attr("src","images/"+(i%3)+".jpg")
+      .attr("alt","Product "+(i+1))  // Alternate Text (Change to product name)
+      .parent().parent()
+      .find("a.product-name strong")
+      .text("Product "+(i+1))
+      .parent().parent()
+      .find("p.product-price")
+      .text("Php "+((Math.random()*10000).toFixed(2)))
+      .parent()
+      .find("p.product-seller user")
+      .text("User"+(i+1))
+      .parent().parent()
+      .find("p.product-rating rating")
+      .text((Math.random()*100).toFixed(2)+"%");
+    $('#thumbnail').parent().append(thumbnail);
+    thumbnail.attr("id","thumbnail"+i);
+  };
+
+  // Make image to square
+  // var imgWidth = $('.thumbnail').find('img').css("width");
+  // $('.thumbnail').find('img').css("height",imgWidth);
+
+  // Fix height per column
+  var thumbHeight= $('.col-thumbnail').css("height");
+  $('.col-thumbnail').css("max-height",thumbHeight);
+
+  // Hide Image Panel template
+  $('div#thumbnail').css("display","none");
+
+  $('.thumbnail').mouseover(function(){
+    $(this).find('.button-group')
+      .css("display","block")
+      .parent()
+      .find(".hidden-caption")
+      .css("display","block")
+      .parent()
+      .parent()
+      .css("padding",0);
+
+      // Overlap Image Panel on hover
+      $('.thumbnail').css("position","relative");
+      $(this).css("z-index",1000);
+  });
+
+  $('.thumbnail').mouseout(function(){
+    $(this).find('.button-group')
+      .css("display","none")
+      .parent()
+      .css("z-index",0)
+      .find(".hidden-caption")
+      .css("display","none")
+
+      // Return to default
+      $(this).css("z-index",0);
+  });
+
+  $('#product-modal').on('show.bs.modal', function (event) {
+    var thumbnail = $(event.relatedTarget).parent();
+    var modal = $(this);
+
+    modal
+      .find("img")
+      .attr("src", thumbnail.find("img").attr("src"));
+
+
+    var img_width =  thumbnail.find('img').width(); 
+    var img_height = thumbnail.find('img').height();
+    modal.find("img").removeClass("portrait").removeClass("landscape");
+    if(img_height > img_width){
+      modal.find("img").addClass("portrait");
+    }
+    else{
+      modal.find("img").addClass("landscape");
+    }
+
+
+    modal
+      .find(".product-name")
+      .text(thumbnail.find(".product-name").text());
+
+    modal
+      .find(".product-price")
+      .text(thumbnail.find(".product-price").text());
+
+    modal
+      .find(".product-seller user")
+      .text(thumbnail.find(".product-seller user").text());
+
+    modal
+      .find(".product-rating rating")
+      .text(thumbnail.find(".product-rating rating").text());
+  });
+});
